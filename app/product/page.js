c820d259a46b6d8f93038c5e6e4b7144b2c15f63
mydemilade <ayunoluwademilade@gmail.com>
2023-10-28 17:48:12 +0100

    img changed to Image

--- app/product/page.js
+++ app/product/page.js
@@ -1,6 +1,6 @@
 "use client"
 import useCart from "../(store)/store"
-
+import Image from "next/image"
 export default function ProductPage(props) {
     const { searchParams } = props
     const { price_id } = searchParams
@@ -30,7 +30,7 @@ export default function ProductPage(props) {
         <div className="flex flex-col p-4">
             <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1000px] mx-auto">
                 <div className="md:p-2 md:shadow">
-                    <img src={productInfo.images[0]} alt={name} className="w-full h-full object-cover" />
+                    <Image src={productInfo.images[0]} alt={name} className="w-full h-full object-cover" />
                 </div>
                 <div className="flex flex-col gap-2 p-4">
                     <div className="flex md:flex-col md:items-start text-xl  items-center justify-between gap-2">
